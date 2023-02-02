1753c5cf2261df470f79941fe5e4198f7fd2cf3d
Samuel <samuelmaglodji@gmail.com>
2023-02-02 09:31:14 +0000

    projects section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,9 @@
-const navEl = document.querySelector('.navbar');
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 80) {
-        navEl.classList.add('navbar-scrolled');
-    } else if (window.scrollY < 80 ) {
-        navEl.classList.remove('navbar-scrolled');
-    }
-});
+// const navEl = document.querySelector('.navbar');
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY >= 80) {
+//         navEl.classList.add('navbar-scrolled');
+//     } else if (window.scrollY < 80 ) {
+//         navEl.classList.remove('navbar-scrolled');
+//     }
+// });
+//code for project page
