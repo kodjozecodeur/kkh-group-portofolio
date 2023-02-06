198a536c5f067f88a24df462210122f6a83caac9
Samuel <samuelmaglodji@gmail.com>
2023-02-06 11:44:25 +0000

    formations section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,2 @@
-// const navEl = document.querySelector('.navbar');
-// window.addEventListener('scroll', () => {
-//     if (window.scrollY >= 80) {
-//         navEl.classList.add('navbar-scrolled');
-//     } else if (window.scrollY < 80 ) {
-//         navEl.classList.remove('navbar-scrolled');
-//     }
-// });
-//code for project page
+
+AOS.init();
